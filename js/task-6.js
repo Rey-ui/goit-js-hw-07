777fc251f5471858fa60a711e32193d888ccd45f
trubchaninov Andriy 1-8 <a.qwerty0444@gmail.com>
2023-12-16 22:55:49 +0200

    solve problems

--- js/task-6.js
+++ js/task-6.js
@@ -3,6 +3,11 @@ const input = document.querySelector('#controls input[type="number"]');
 const btnCreate = document.querySelector("[data-create]");
 const btnDestroy = document.querySelector("[data-destroy]");
 const boxes = document.querySelector("#boxes");
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+}
 function createBoxes(amount) {
     for (let i = 0; i < amount; i++) {
         const boxSize = 30 + i * 10;
